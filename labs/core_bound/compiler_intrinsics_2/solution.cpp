a85c723359f52ea443d66964cad33ad27b796903
stdbug <68459057+stdbug@users.noreply.github.com>
2022-07-05 00:33:39 +0200

    Can we make it without intrinsics?

--- labs/core_bound/compiler_intrinsics_2/solution.cpp
+++ labs/core_bound/compiler_intrinsics_2/solution.cpp
@@ -1,11 +1,43 @@
 #include "solution.hpp"
 
+#include <cstdint>
 #include <iostream>
 
+inline uint64_t PopCnt(uint64_t n) {
+#ifdef _MSC_VER
+  return __popcnt64(n);
+#else
+  return __builtin_popcountll(n);
+#endif
+}
 
-bool ContainsZeroByte(uint64_t v) {
+inline bool ContainsZeroByte(uint64_t v) {
   return ((v - UINT64_C(0x0101010101010101)) & ~v &
           UINT64_C(0x8080808080808080)) != 0;
+}
+
+inline uint64_t NLZ(uint64_t x) {
+  x = x | (x >> 1);
+  x = x | (x >> 2);
+  x = x | (x >> 4);
+  x = x | (x >> 8);
+  x = x | (x >> 16);
+  x = x | (x >> 32);
+
+  return PopCnt(~x);
+}
+
+inline uint64_t NTZ(uint64_t x) {
+  constexpr uint64_t kMod37BitPosition[] = {
+      32, 0,  1,  26, 2,  23, 27, 0,  3, 16, 24, 30, 28, 11, 0,  13, 4,  7, 17,
+      0,  25, 22, 31, 15, 29, 10, 12, 6, 0,  21, 14, 9,  5,  20, 8,  19, 18};
+  if ((x & UINT64_C(0xFFFFFFFF)) == 0) {
+    uint32_t v = x >> 32;
+    return 32 + kMod37BitPosition[(-v & v) % 37];
+  } else {
+    uint32_t v = x;
+    return kMod37BitPosition[(-v & v) % 37];
+  }
 }
 
 unsigned solution(const std::string& inputContents) {
@@ -32,17 +64,22 @@ unsigned solution(const std::string& inputContents) {
 
   for (const uint64_t* pu64 = pu64_beg; pu64 < pu64_end; pu64++) {
     constexpr uint64_t kMask = UINT64_C(0x0A0A0A0A0A0A0A0A);
-    uint64_t u64 = *pu64;
-    if (!ContainsZeroByte(u64 ^ kMask)) {
+    const uint64_t u64 = *pu64;
+    const uint64_t masked = u64 ^ kMask;
+    if (!ContainsZeroByte(masked)) {
       continue;
     }
+    const uint64_t tmp = ((masked & UINT64_C(0x7F7F7F7F7F7F7F7F)) +
+                          UINT64_C(0x7F7F7F7F7F7F7F7F)) &
+                         0x8080808080808080;
+    const uint64_t x80ed = ~(tmp | masked | UINT64_C(0x7F7F7F7F7F7F7F7F));
     const char* pch = reinterpret_cast<const char*>(pu64);
-    for (int i = 0; i < 8; i++) {
-      if (pch[i] == '\n') {
-        longest = std::max<ptrdiff_t>(longest, &pch[i] - last_line_beg);
-        last_line_beg = &pch[i + 1];
-      }
+    ptrdiff_t lsf = pch - last_line_beg;
+    if (lsf + 8 >= longest) {
+      longest = std::max<ptrdiff_t>(longest, lsf + NTZ(x80ed) / 8);
     }
+
+    last_line_beg = pch + 8 - NLZ(x80ed) / 8;
   }
 
   const char* end_aligned = reinterpret_cast<const char*>(u64end_aligned);
